944623b44a5ea193bba919cf8a2d341a13f58f62
arnel.aves <arnel.deo.aves@gmail.com>
2020-07-14 14:09:15 +0800

    Modified the output of the console debugger

--- InforCustomTypesDebugger/Program.cs
+++ InforCustomTypesDebugger/Program.cs
@@ -18,12 +18,19 @@ namespace InforCustomTypesDebugger
         {
             StringBuilder sBuilder = new StringBuilder();
 
+            // getting an existing type with alias "Account"
+            var objAccount = Infor.CustomTypes.Utilities.CustomTypesCollection.GetCustomType("account");
+            sBuilder.AppendLine("Return for using the \"Account\" alias : " + objAccount.ToString());
 
-            var obj = Infor.CustomTypes.Utilities.CustomTypesCollection.GetCustomType("account");
-            sBuilder.AppendLine(obj.ToString());
+            // getting an existing type with alias "Address"
+            var objAddress = Infor.CustomTypes.Utilities.CustomTypesCollection.GetCustomType("address");
+            sBuilder.AppendLine("Return for using the \"Address\" alias : " + objAddress.ToString());
+
+            // getting an non existing type
+            var objNotFound = Infor.CustomTypes.Utilities.CustomTypesCollection.GetCustomType("notfound");
+            sBuilder.AppendLine("Return for using the \"NotFound\" alias : " + objNotFound.ToString());
 
             Console.WriteLine(sBuilder.ToString());
-
 
         }
 
